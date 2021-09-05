f7d01fae983bbdb18ace3b505911a5162bd7d616
harun ugur <ugur.harun@yandex.com>
2021-09-05 12:15:12 +0300

    serve a struct example

--- api.go
+++ api.go
@@ -6,7 +6,12 @@ import (
 	"net/http"
 )
 
-func hello(w http.ResponseWriter, r *http.Request)  {
+type Employee struct {
+	Name     string
+	LastName string
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
 	_, err := fmt.Fprintf(w, "Hello %v", r.URL.Path[1:])
 	if err != nil {
 		log.Println("Error occurred", r.URL)
@@ -14,12 +19,42 @@ func hello(w http.ResponseWriter, r *http.Request)  {
 	}
 }
 
-func main()  {
+var html = "<table>" +
+	"<tr>" +
+	"<th>Name</th>" +
+	"<th>Last Name</th>" +
+	"</tr>" +
+	"<tr>" +
+	"<td>%v</td>" +
+	"<td>%v</td>" +
+	"</tr>" +
+	"<table>"
 
-	http.HandleFunc("/",hello)
+func (e *Employee) employeeInfo(w http.ResponseWriter, r *http.Request) {
+	e.Name = "Harun"
+	e.LastName = "Ugur"
+	err := r.ParseForm()
+	if err != nil {
+		log.Println("Error occurred", err)
+		return
+	}
+	fmt.Println(r.Form)
+	fmt.Println("path:", r.URL.Path)
+	_, err = fmt.Fprintf(w, html, e.Name, e.LastName)
+	if err != nil {
+		log.Println("Error occurred", err)
+		return
+	}
+}
+
+func main() {
+	e := new(Employee)
+	http.HandleFunc("/", hello)
+	http.HandleFunc("/employee", e.employeeInfo)
+
 	err := http.ListenAndServe(":8081", nil)
 	if err != nil {
-		log.Println("Webservice failed",err)
+		log.Println("Webservice failed", err)
 		return
 	}
 	fmt.Println("-----Webservice started-----")
